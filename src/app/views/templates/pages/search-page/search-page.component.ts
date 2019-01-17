26ea2eb2a44673eff2ff1e591b5a1c8434d57da8
STeven <sanguine.steve@gmail.com>
2019-01-17 08:26:38 +0200

    Added Debounce timer

--- src/app/views/templates/pages/search-page/search-page.component.ts
+++ src/app/views/templates/pages/search-page/search-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 import { GithubService } from '../../../../service/github.service';
 
 @Component({
@@ -6,28 +9,43 @@ import { GithubService } from '../../../../service/github.service';
   templateUrl: './search-page.component.html',
   styleUrls: ['./search-page.component.css']
 })
-export class SearchPageComponent {
-  constructor(private githubService: GithubService) {}
+export class SearchPageComponent implements OnInit {
 
-  // Get input value from local reference
-  @ViewChild('searchInput') searchInputRef: ElementRef;
+  formGroup: FormGroup;
+  subject: Subject<any> = new Subject();
+
+  constructor(private githubService: GithubService) {}
 
   // Used for the for loop once API data has been assigned.
   public repo = [];
   public errorMsg;
+
+  // Get input value from local reference
+  @ViewChild('searchInput') searchInputRef: ElementRef;
+
+  ngOnInit() {
+
   /*
-  Method: Search Github API on input Keyup property
-  Assign returned data to 'repo' property
+  Search Github API on input Keyup property
+  Assign returned data to 'repo' property after debounce timer
   */
+ this.repo = [];
+
+ // Subscribe to the observable in the githubService
+ this.subject
+   .pipe(debounceTime(1000))
+   .subscribe(() => {
+   this.githubService
+     .searchRepo(this.searchInputRef.nativeElement.value)
+     .subscribe(data => this.repo = data,
+     error => this.errorMsg = error
+      );
+    });
+  }
+
+
   searchRepo() {
     // Reset repo property
-    this.repo = [];
-
-    // Subscribe to the observable in the githubService
-    this.githubService
-      .searchRepo(this.searchInputRef.nativeElement.value)
-      .subscribe(data => this.repo = data,
-      error => this.errorMsg = error
-    );
+    this.subject.next();
   }
 }
